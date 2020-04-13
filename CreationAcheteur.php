b9554c58c6790b4351a93cd3e1dab7c6fd4cd0f5
Victor Quidet <victor.quidet@gmail.com>
2020-04-13 16:00:50 +0200

    version moche mais qui marche bien pour gestion des comptes acheteurs

---
+++ CreationAcheteur.php
@@ -0,0 +1,57 @@
+<?php
+
+$prenom = isset($_POST["Prénom"])? $_POST["Prénom"] : ""; 
+$nom = isset($_POST["Nom"])? $_POST["Nom"] : "";
+$login = isset($_POST["Identifiant"])? $_POST["Identifiant"] : "";
+$email = isset($_POST["email"])? $_POST["email"] : "";
+$pwd = isset($_POST["pwd"])? $_POST["pwd"] : "";
+$pwd2 = isset($_POST["pwd2"])? $_POST["pwd2"] : "";
+$cgv = isset($_POST["CGV"])? $_POST["CGV"] :"";
+$erreur = "";
+
+$database = "ebayece";
+
+$db_handle = mysqli_connect('localhost:3308', 'root', '');
+$db_found = mysqli_select_db($db_handle, $database);
+
+if($db_found){
+
+
+ if ($prenom == "") { 
+ $erreur .= "Prénom est vide. <br>";
+ }
+ if ($nom == "") {
+ $erreur .= "Nom est vide. <br>"; 
+}
+ if ($cgv =="") {
+     $erreur .= "Merci d'accepter les conditions générales de ventes. <br>";
+ }
+ if ($login == "") {
+ $erreur .= "Identifiant est vide. <br>"; 
+}
+ if ($email == "") {
+ $erreur .= "E-mail est vide. <br>"; 
+}
+ if ($pwd == "") {
+    $erreur .= "Mot de passe est vide. <br>"; 
+}
+}
+if ($pwd != $pwd2) {
+    $erreur .= "Les mots de passes sont différents. <br>";
+} 
+
+if ($erreur == "") {
+    $sqlInsert = "INSERT INTO acheteurs (login, pwd, Nom, Prenom, email) VALUES ('$login', '$pwd', '$nom', '$prenom', '$email')";
+    $result = mysqli_query($db_handle, $sqlInsert);
+    if (!$result){
+        die("impossible d ajouter cet enregistrement");
+    }
+    echo "Votre compte a bien été créer !";
+}
+    else {
+    echo "Erreur : $erreur";
+    include('CreationAcheteur.html');
+    exit; 
+    }
+
+?>
