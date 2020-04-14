47a8f779f39d0ab0871a0f1212554ea8a92cd0b4
Victor Quidet <victor.quidet@gmail.com>
2020-04-14 22:02:31 +0200

    l'index et le menu à jour

---
+++ PROJET/index.php
@@ -0,0 +1,67 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ebay ECE</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1"> 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+</head> 
+<body>
+   
+        <?php 
+            include ("menu.php");
+        ?>
+       
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="images/1.jpg" alt="First slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="images/2.jpg" alt="Second slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="images/3.jpg" alt="Third slide">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <footer class="page-footer"> 
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-md-8 col-sm-12">
+                <h6 class="text-uppercase font-weight-bold">Information additionnelle</h6> 
+                <p>
+                    Blablalalalalalallalalalalalaalallalallalalalallalalalallalaallalal</p>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12">
+                    <h6 class="text-uppercase font-weight-bold">Contact</h6> 
+                    <p>
+                        Blabla <br> email <br>
+                        number <br>
+                        number
+                    </p> 
+                </div>
+            </div>
+        <div class="footer-copyright text-center">&copy; 2020 Copyright | Droit d'auteur: Paul SENARD | Victor QUIDET | Solène HACOUN</div> 
+        </div>
+    </footer>
+</body>
+</html>
---
+++ PROJET/menu.php
@@ -0,0 +1,84 @@
+<?php
+session_start();
+//echo $_SESSION['id'];
+?>
+   <link rel="stylesheet" type="text/css" href="css/styles.css"> 
+<nav class="navbar navbar-expand-md">
+        <a class="navbar-brand" href="#">Vendre&ensp;<i class="fa fa-money" aria-hidden="true"></i></a>
+        <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="main-navigation">
+
+            
+                <?php 
+                if (isset($_SESSION['statut']) && $_SESSION['statut'] == "membre")
+                {
+                   echo'<ul class="navbar-nav">
+                   <li class="nav-item"><a class="nav-link" href="#">
+                       Votre Compte&ensp; <i class="fa fa-user" aria-hidden="true"></i></a></li>
+                   <li class="nav-item"><a class="nav-link">|</a></li>
+                   <li class="nav-item"><a class="nav-link" href="#">
+                       Panier&ensp; <i class="fa fa-shopping-basket" aria-hidden="true"></i></a></li>
+                   <li class="nav-item"><a class="nav-link">|</a></li>
+                   <li class="nav-item"><a class="nav-link" href="admin.php">Admin</a></li>
+               </ul>';
+                }
+                elseif(isset($_SESSION['statut']) && $_SESSION['statut'] == "administrateur"){
+                    echo '<ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link" href="login.html">
+                        Votre Compte&ensp; <i class="fa fa-user" aria-hidden="true"></i></a></li>
+                    <li class="nav-item"><a class="nav-link">|</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">
+                        Panier&ensp; <i class="fa fa-shopping-basket" aria-hidden="true"></i></a></li>
+                    <li class="nav-item"><a class="nav-link">|</a></li>
+                    <li class="nav-item"><a class="nav-link" href="adminPage.php">Admin</a></li>
+                </ul> ';
+                }
+                else{
+                    echo '<ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link" href="login.html">
+                        Votre Compte&ensp; <i class="fa fa-user" aria-hidden="true"></i></a></li>
+                    <li class="nav-item"><a class="nav-link">|</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">
+                        Panier&ensp; <i class="fa fa-shopping-basket" aria-hidden="true"></i></a></li>
+                    <li class="nav-item"><a class="nav-link">|</a></li>
+                    <li class="nav-item"><a class="nav-link" href="admin.php">Admin</a></li>
+                </ul> ';
+
+                }
+                ?>
+                 </div>
+    </nav>
+    <div>
+        <a href="index.php"><img src="images/ebay-logo.png" class="logo-accueil"></a>
+    </div>
+    <nav class="navbar navbar-expand-md navbar-light">
+        <div class="navbar-second">
+            <hr>
+            <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Accueil&ensp;<i class="fa fa-home" aria-hidden="true"></i><span class="sr-only">Accueil</span></a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Catégories
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="#">Ferraille ou Trésor</a>
+                <a class="dropdown-item" href="#">Bon pour le Musée</a>
+                <a class="dropdown-item" href="#">Accessoire VIP&ensp;<i class="fa fa-star" aria-hidden="true"></i></a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Autres Catégories</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Achat&ensp;<i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Les Meilleures Ventes&ensp;<i class="fa fa-heart" aria-hidden="true"></i></a>
+            </li>
+        </ul><hr>
+        </div>
+    </nav>
+
