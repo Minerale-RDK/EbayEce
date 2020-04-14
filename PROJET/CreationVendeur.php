95482bfd187344a5160d8d7b1b052c7f56c4417e
paulsenard <47627695+paulsenard@users.noreply.github.com>
2020-04-14 20:03:40 +0200

    Update CreationVendeur.php

--- PROJET/CreationVendeur.php
+++ PROJET/CreationVendeur.php
@@ -7,9 +7,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1"> <link rel="stylesheet"
     href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="CreationAcheteur.css" />
 </head>
+
 <body>
     <div class="col-9" id="Cadre1"> 
         <div calss="col-7" id="Cadre2"> 
@@ -35,26 +37,46 @@
                         <div> 
                             Avatar :  
                             <input size="100" type="file" name="fichier" value="" />
-                            <input type="submit" value="Envoyez le fichier" /><br><br>
                         </div>
                         <br><p>Choisissez votre fond préféré</p>
                         <div class="row">
-                        <div class="col-4"><br>
-                            <div class="list-group" id="list-tab" role="tablist">  
-                                <a type="radio" class="list-group-item list-group-item-action active" id="couv1" name="couv1" data-toggle="list" href="#list-home" role="tab" aria-controls="home" value="1">Automne</a>
-                                <a type="radio" class="list-group-item list-group-item-action" id="couv2" name="couv2" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile" value="2">Wallpaper</a>
-                                <a type="radio" class="list-group-item list-group-item-action" id="couv3" name="couv3" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages" value="3">Flou</a>
-                                <a type="radio" class="list-group-item list-group-item-action" id="couv4" name="couv4" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings" value="4">Etoiles</a>
+                            <div class="col-lg-3 col-md-3 col-sm-12">
+                                <div class="cover"><img src="images/fonddecouv/1.jpg" alt="automne"></div>
+                                <div class="form-check">
+                                <input class="form-check-input" type="radio" name="cover" id="cover" value="images/fonddecouv/1.jpg" checked>
+                                <label class="form-check-label" for="cover">
+                                    Couverture Automne
+                                </label>
+                                </div>                                 
                             </div>
-                        </div>
-                        <div class="col-4ol">
-                            <div class="tab-content" id="nav-tabContent">
-                            <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list"><img src="images/fonddecouv/1.jpg" alt="automne"></div>
-                            <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list"><img src="images/fonddecouv/5.jpg" alt="Wallpaper"/></div>
-                            <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list"><img src="images/fonddecouv/3.jpg" alt="Flou"></div>
-                            <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list"><img  src="images/fonddecouv/4.jpg" alt="Etoiles"></div>
-                            </div><br>
-                        </div>
+                            <div class="col-lg-3 col-md-3 col-sm-12"> 
+                                <div class="cover"><img src="images/fonddecouv/5.jpg" alt="Wallpaper"></div>
+                                <div class="form-check">
+                                <input class="form-check-input" type="radio" name="cover" id="cover" value="images/fonddecouv/5.jpg" checked>
+                                <label class="form-check-label" for="cover">
+                                    Couverture Stylisée
+                                </label>
+                                </div>                      
+                            </div>
+                            <div class="col-lg-3 col-md-3 col-sm-12">  
+                                <div class="cover"><img src="images/fonddecouv/3.jpg" alt="Flou"></div>
+                                <div class="form-check">
+                                <input class="form-check-input" type="radio" name="cover" id="cover" value="images/fonddecouv/3.jpg" checked>
+                                <label class="form-check-label" for="cover">
+                                    Couverture Gouttes
+                                </label>
+                                </div>   
+                            </div>
+                            <div class="col-lg-3 col-md-3 col-sm-12">  
+                                <div class="cover"><img  src="images/fonddecouv/4.jpg" alt="Etoiles"></div>  
+                                <div class="form-check">
+                                <input class="form-check-input" type="radio" name="cover" id="cover" value="images/fonddecouv/4.jpg" checked>
+                                <label class="form-check-label" for="cover">
+                                    Couverture Etoiles
+                                </label>
+                                </div> 
+                            </div>
+                            <br>
                         </div>
                         <div>
                             <input type="checkbox" id="CGV" name="CGV" value="CGV">
