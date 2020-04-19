c19f10a667f65a87eac2056030de3542c73d2451
paulsenard <47627695+paulsenard@users.noreply.github.com>
2020-04-20 00:42:35 +0200

    panier correct

--- PROJET/items/addpanier.php
+++ PROJET/items/addpanier.php
@@ -23,12 +23,19 @@ if ($db_found)
     }
     $id = $_GET['id'];
     if(isset($_GET['id'])){
+        for($i=0; $i<sizeof($_SESSION['panier']); $i++){
+            if ($id == $_SESSION['panier'][$i]['IDItem']){
+                echo '<div class="alert alert-danger" role="alert"> Ce produit est déjà dans votre panier </div>';
+                include('produit.php');
+                exit();
+            }
+        }
         $sql = "SELECT * FROM items WHERE IDItem = '$id'";
         $req = mysqli_query($db_handle, $sql);
         $data = mysqli_fetch_assoc($req);
         $result = mysqli_query($db_handle, $sql);
         if (mysqli_num_rows($result) == 0) {}
-        $taille = sizeof($_SESSION['panier']) + 1;
+        $taille = sizeof($_SESSION['panier']);
         $donnees_item = array();
         $tampon = [];
         foreach($valeurs as &$valeur){
@@ -37,9 +44,10 @@ if ($db_found)
 
         }
         $_SESSION['panier'][$taille] = $tampon;
-        print_r($_SESSION['panier'][$taille]['IDItem']);
 
-        die("Le produit a bien été ajouté à votre panier");
+        echo '<div class="alert alert-success" role="alert"> Ce produit est déjà dans votre panier </div>';
+        include('produit.php');
+        exit();
     }else {
         die("Vous n'avez pas sélectionné de produits");
     }
