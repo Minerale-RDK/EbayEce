4ff14b83a9e5430687f0fa5d6e2c0b6b3d9c27d9
paulsenard <47627695+paulsenard@users.noreply.github.com>
2020-04-19 21:44:03 +0200

    footer nickel

--- PROJET/bases/footer.php
+++ PROJET/bases/footer.php
@@ -1,24 +1,55 @@
-
-
 <footer class="page-footer" style="margin-top :100px">
+      
         
-<div class="container" style="margin-top :30px ; height:155px" >
-<div class="row">
-    <div class="col-lg-8 col-md-8 col-sm-12" style="margin-top :30px">
-    <h6 class="text-uppercase font-weight-bold">Information additionnelle</h6> 
-    <p> Blablalalala</p>
-    </div>
+<div class="container" style="margin-top :40px ; height:160px" >
+        <div class="row" >
+            <div class="col" style="margin-top :30px">
+                <p style="text-align: center">
+                <a href="../items/articles.php?cat=FoT" style="color: white">Ferraille ou Trésors</a>
+                <br>
+                <a href="../items/articles.php?cat=BpM" style="color: white">Bons pour le musée</a>
+                <br>
+                <a href="../items/articles.php?cat=vip" style="color: white">Accessoirs VIP</a>
+                <br>
+                <a href="#" style="color: white">Revenir en haut de la page</a>
+                    </p>
+            </div>
+            <div class="col" style="margin-top :45px">
+                <!--<h6 class="text-uppercase font-weight-bold" style="text-align: center">Informations supplémentaires</h6>-->
+                <p style="text-align: center">
+                <a href="regles.php" style="color: white">Réglementations du site</a>
+                <br>
+                <a href="vendre.php" style="color: white">Comment vendre</a>
+                <br>
+                <a href="soucres.php" style="color: white">Nos sources</a>
+                    </p>
+            </div>
     
-    <div class="col-lg-4 col-md-4 col-sm-12" style="margin-top :30px">
-        <h6 class="text-uppercase font-weight-bold">Contact</h6> 
-        <p>
-            Blabla <br> 
-            email</p> 
+
+            
+            <div class="col" style="margin-top :53px">
+              <!--  <h6 class="text-uppercase font-weight-bold" style="text-align: center">Contact</h6> -->
+                <p style="text-align: center">
+                  
+         <!--       <div id="content">
+		<div id="info">-->
+			
+			<a href="tel:0664836555" style="color: white">Appelez nous : 0664836555</a><!--  ouvre un lien qui envoie vers une appli qui appelle le num  -->
+			<br>
+			<a href="mailto:encheres.ece@gmail.com?Subject=Webcontact" style="color: white")>Enchères.ece@gmail.com</a>
+
+<!--		</div>-->
+                
+                
+                </p> 
+            </div>
+            
+        </div>
     </div>
-</div>
-<div class="footer-copyright text-center">&copy; 2020 Copyright | Droit d\'auteur: Paul SENARD | Victor QUIDET | Solène HACOUN
-</div> 
-</div>
+        <div  style="background-color : ghostwhite" class="footer-copyright text-center ; " >&copy; 2020 Copyright | Droit d'auteur: Paul SENARD | Victor QUIDET | Solène HACOUN
+         
+        </div>
+            
 
 
 </footer>
